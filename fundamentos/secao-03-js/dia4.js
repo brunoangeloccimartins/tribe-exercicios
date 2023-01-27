b5caef3a2fc9973cb385764232aef0c23241710f
brunoangeloccimartins <brunoangelocci@gmail.com>
2023-01-27 17:54:57 -0300

    Exercícios arrow functions etc

---
+++ fundamentos/secao-03-js/dia4.js
@@ -0,0 +1,67 @@
+//exercicio
+
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
+    
+//   }
+//   imprimeIdade()
+
+//exercicio
+
+
+//   const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   pessoa.nome = 'Luna'
+//   pessoa.idade = 19
+
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
+
+//exercicio
+
+// let favoriteFood = 'Lasanha';
+// favoriteFood = 'Hamburguer';
+// console.log(favoriteFood);
+
+//exercicio
+
+// const name = 'Adriana';
+// const lastName = 'Soares';
+// console.log(`Olá, ${name} ${lastName}!`);
+// function soma(a,b) {
+//   let resultado = a + b;
+//   return resultado;
+// }
+// let a = 3;
+// let b = 5;
+// console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
+
+//exercicio
+
+// const numeroAleatorio = () => Math.random() 
+// console.log(numeroAleatorio());
+
+// exercicio
+
+// const hello = (nome) =>  `Olá, ${nome}!`
+//   let nome = 'Ivan';
+//   console.log(hello(nome));
+
+//exercicio
+
+// const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
+//   let nome = 'Ivan';
+//   let sobrenome = 'Pires';
+//   console.log(nomeCompleto(nome, sobrenome));
+
+//exercicio
+
+// let speed = 90;
+// const speedCar = (speed) => (
+//   speed >= 120 ? `Você ultrapassou o limite de velocidad` : `Você está na velocidade permitida`
+// );
+// console.log(speedCar(speed));
